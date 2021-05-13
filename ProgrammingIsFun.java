de3d0b8b33bbbccfcc229d1b980ed4e5defb79e4
Skhosana07 <73025462+Skhosana07@users.noreply.github.com>
2021-05-13 15:36:47 +0200

    Add files via upload

---
+++ ProgrammingIsFun.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ProgrammingIsFun
+{
+	
+	public static void main( String[] args)
+	{
+
+		JOptionPane.showMessageDialog( null, "Programming is Fun!");
+	}
+}
+
+
